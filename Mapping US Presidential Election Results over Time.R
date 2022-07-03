57d746d121929e19b8aea26481154eca86d19537
vkataria6 <106615136+vkataria6@users.noreply.github.com>
2022-07-03 19:55:38 -0400

    Add files via upload

--- Mapping US Presidential Election Results over Time.R
+++ Mapping US Presidential Election Results over Time.R
@@ -1,4 +1,3 @@
-# question-1-mapping-elections
 ```{r}
 par(cex = 1.5)
 library(maps)
@@ -18,7 +17,6 @@ election <- election %>%
   filter(year == 2008) %>%
   left_join(fips, by = "county")
 
-
 ## coloring based on two-party vote share
 ### If a value is greater than 0 in a county, the Republican candidate received more votes in the county.
 ### If a value is smaller than 0 in a county, the Democratic candidate received more votes in the county.
@@ -29,8 +27,6 @@ election <- data.frame(election) %>%
   mutate("dem_share" = dem/total) %>%
   mutate("value" = rep_share - dem_share) 
 
-
-
 ## initialize a map for Arizona
 electionAZ <- election %>%
   filter(state == "arizona") %>%
@@ -38,10 +34,10 @@ electionAZ <- election %>%
 
 
 AZ <- county_choropleth(electionAZ,
-                  title      = "Outcome of the 2008 US Presidential Election: Arizona",
-                  legend     = "value",
-                  num_colors = 1,
-                  state_zoom = "arizona")
+                        title      = "Outcome of the 2008 US Presidential Election: Arizona",
+                        legend     = "value",
+                        num_colors = 1,
+                        state_zoom = "arizona")
 
 AZ + scale_fill_gradient2(
   low = "#0033FF",
@@ -53,7 +49,6 @@ AZ + scale_fill_gradient2(
   coord_map("ortho", orientation = c(40, -100, 0))+
   theme_minimal()
 
-
 ## initialize a map for Massachusetts
 electionMA <- data.frame(election) %>%
   filter(state == "massachusetts") %>%
@@ -61,10 +56,10 @@ electionMA <- data.frame(election) %>%
 
 
 MA <- county_choropleth(electionMA,
-                  title      = "Outcome of the 2008 US Presidential Election: Massachusetts",
-                  legend     = "value",
-                  num_colors = 1,
-                  state_zoom = "massachusetts")
+                        title      = "Outcome of the 2008 US Presidential Election: Massachusetts",
+                        legend     = "value",
+                        num_colors = 1,
+                        state_zoom = "massachusetts")
 
 MA + scale_fill_gradient2(
   low = "#0033FF",
@@ -77,4 +72,6 @@ MA + scale_fill_gradient2(
   theme_minimal()
 
 ```
+
 ## My comment
+All of Massachusetts countries voted overwhelmingly in favor of Barack Obama in 2008. However, the political tendencies of Arizonas counties ranged widely. Only two countries were McCains strongholds when compared to Obamas in the general election. Obama won four counties in Arizona against McCain, which is surprising condisering that McCain was known as the "home of turf" of Arizona. 
